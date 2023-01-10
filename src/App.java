196ff081590a7890a068859ca66999ec40051c65
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-10 14:34:47 -0300

    feat: create logic to print table

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Qual tabela você gostaria de ver?");
+        Scanner scan = new Scanner(System.in);
+        int baseNumber = scan.nextInt();
+        scan.close();
+        System.out.println("Tabela de multiplicação de " + baseNumber);
+        for (int i = 1; i <= 10; i++) {
+            int multiplication = baseNumber * i;
+            System.out.println(baseNumber + " X " + i + " = " + multiplication);
+        }
     }
 }
